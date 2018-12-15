f82901178adef066846d712d2436a850962ea7ca
lukasz_brainhub <lukasz@brainhub.pl>
2018-12-15 19:02:51 +0100

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # papu-api
+
+### Environment variables
+- **PORT** (default: 8080)
+- **DB_NAME** (default: 'papu')
+- **DB_HOST** (default: 'localhost:27017')
+- **ADMIN_PASSWORD** - password then required to access API
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
   port: process.env.PORT || 8080,
   bodyLimit: '100kb',
   corsHeaders: ['Link', 'Jwt'],
-  dbName: 'papu',
+  dbName: process.env.DB_NAME || 'papu',
   dbHost: process.env.DB_HOST || 'localhost:27017',
   password: process.env.ADMIN_PASSWORD,
 };
